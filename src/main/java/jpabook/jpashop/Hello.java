d49bf89b302c6a2f3582b8fc7a5edb0167638991
Gatsjy <gkswndks123@naver.com>
2020-10-10 22:33:08 +0900

    2020-10-10, 한주안, 실전편 윈도우로 다시 시작

---
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author Gatsjy
+ * @since 2020-10-10
+ * realize dreams myself
+ */
+@Getter @Setter
+public class Hello {
+    private String data;
+}
